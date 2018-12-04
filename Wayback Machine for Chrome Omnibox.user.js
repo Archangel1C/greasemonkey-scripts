c5a43b842d5d340cfb1d9c6c9149af9686d89ac7
Archangel1C <12956838+Archangel1C@users.noreply.github.com>
2018-12-04 12:41:14 +0100

    add version 0.2 of the script

---
+++ Wayback Machine for Chrome Omnibox.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Wayback Machine for Chrome Omnibox
+// @namespace    https://github.com/Archangel1C
+// @version      0.2
+// @description  Revert the URL encoding of colons by Chrome %s replacement to make it work with Wayback Machines search
+// @author       Archangel1C
+// @match        http*://web.archive.org/web/*
+// @grant        none
+// ==/UserScript==
+//
+// Sources/Influences:
+// - https://superuser.com/a/302179 for the JavaScript part
+// - https://superuser.com/a/364060 for the GreaseMonkey part
+
+(function() {
+// debugger instruction lets Chrome stop here
+//  debugger;
+
+    // https://www.w3schools.com/js/js_strict.asp
+    // https://stackoverflow.com/a/1335881/4423698
+    'use strict';
+
+    // avoid infinite recursion
+    if(window.location.href.search(/%3A/i) > -1) {
+        window.location = window.location.href.replace(/%3A/g, ":");
+    }
+})();
